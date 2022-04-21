58e98aec14603266aac6f658dce25fbe38a81cb5
xsh <xsh@xsh.local>
2022-04-21 16:02:42 +0800

    feat: 容器和具体页面模块可以融合啦

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,28 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
+  pages: ["__pages/index/index",
+    'pages/index/index',
+    'pages/home/index'
   ],
   window: {
     backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
+    navigationBarBackgroundColor: '#f0f',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+
+  tabBar: {
+    "color": "#000",
+    "borderStyle": "white",
+    "selectedColor": "#0f0",
+    "list": [
+      {
+        "pagePath": "pages/home/index",
+        "text": "home222"
+      },
+      {
+        "pagePath": "pages/index/index",
+        "text": "index111"
+      }
+    ]
   }
 })
