56360f8016a5fc5f745b359685e67501007f68a2
xsh <xsh@xsh.local>
2022-04-22 10:51:23 +0800

    chore: 临时源码目录迁移至当前用户目录下

--- template/app.config.ts
+++ template/app.config.ts
@@ -1,28 +1,9 @@
 export default defineAppConfig({
-  pages: ["__pages/index/index",
-    'pages/index/index',
-    'pages/home/index'
-  ],
+  pages: ["__pages/index/index"],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#f0f',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  },
-
-  tabBar: {
-    "color": "#000",
-    "borderStyle": "white",
-    "selectedColor": "#0f0",
-    "list": [
-      {
-        "pagePath": "pages/home/index",
-        "text": "home222"
-      },
-      {
-        "pagePath": "pages/index/index",
-        "text": "index111"
-      }
-    ]
   }
 })
